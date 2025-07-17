a5e741cff831d06c62588fe020759dc26fd67155
Devprodipraj <devprodipraj@gmail.com>
2025-07-18 00:44:48 +0600

    comment some code for testing in vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from "react";
-import HeroArea from "@/component/home/HeroArea";
+// import HeroArea from "@/component/home/HeroArea";
 import TopProduct from "@/component/home/TopProduct";
 import SpecialProducts from "@/component/home/SpecialProducts";
 import ShowCase from "@/component/home/ShowCase";
